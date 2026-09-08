68ec56b1a346281cd8a4a552dc757a3cdfe60c85
Sebastian Cloos Hylander <sehy@itu.dk>
2026-09-08 10:52:05 +0200

    file-explorer reads and shows working dir

--- src/main.rs
+++ src/main.rs
@@ -170,22 +170,7 @@ struct Explorer {
     file_explorer: FileExplorer
 } 
 impl Explorer {
-    fn new(dir_path: PathBuf) -> io::Result<Self> {
-        if !dir_path.exists() {
-            return Err(io::Error::new(
-                io::ErrorKind::NotFound,
-                "directory does not exist",
-            ));
-        } 
-        if !dir_path.is_dir() {
-            return Err(io::Error::new(
-                io::ErrorKind::NotADirectory,
-                "path is not a directory",
-            ));
-        }
-
-        let mut file_explorer = FileExplorerBuilder::build_with_working_dir(dir_path.clone()).unwrap();
-
+    fn new(dir_path : PathBuf, mut file_explorer : FileExplorer) -> io::Result<Self> {
         let title = String::from(dir_path.clone().file_stem().unwrap().to_str().unwrap());
 
         let theme = Theme::default().with_title_top(move |_| {format!("{}/",  title.clone()).into()});
@@ -196,7 +181,26 @@ impl Explorer {
             root_dir: dir_path,
             file_explorer: file_explorer
         })
-        
+    }
+
+    fn new_from_root_dir(dir_path: PathBuf) -> io::Result<Self> {
+        if !dir_path.is_dir() {
+            return Err(io::Error::new(
+                io::ErrorKind::NotADirectory,
+                "path is not a directory",
+            ));
+        }
+        Self::new(dir_path.clone(), FileExplorerBuilder::build_with_working_dir(dir_path).unwrap())
+    }
+
+    fn new_from_file(file_path: PathBuf) -> io::Result<Self> {
+        if !file_path.is_file() {
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "path is not a file",
+            ));
+        }
+        Self::new(file_path.clone().parent().unwrap().to_path_buf(), FileExplorerBuilder::build_with_working_file(file_path).unwrap())
     }
 }
 
@@ -213,7 +217,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             if path.is_dir() {
                 let mut editor = Editor::new(None)?;
 
-                let mut explorer = Explorer::new(path)?;
+                let mut explorer = Explorer::new_from_root_dir(path)?;
 
                 enable_raw_mode()?;
 
@@ -241,7 +245,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             } else {
                 let mut editor = Editor::new(Some(path.clone()))?;
 
-                let mut explorer = Explorer::new(path.parent().unwrap().to_path_buf())?;
+                let mut explorer = Explorer::new_from_file(path)?;
 
                 enable_raw_mode()?;
 
